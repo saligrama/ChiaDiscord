72ca09e845f3806b9166dbfb6c93faa0acdd858d
Aditya Saligrama <saligrama427@gmail.com>
2018-03-01 12:24:25 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,58 @@
+// Load up the discord.js library
+const Discord = require("discord.js");
+
+// This is your client. Some people call it `bot`, some people call it `self`,
+// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
+// this is what we're refering to. Your client.
+const client = new Discord.Client();
+
+// Here we load the config.json file that contains our token and our prefix values.
+const auth = require("./auth.json");
+// config.token contains the bot's token
+// config.prefix contains the message prefix.
+
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setGame("Ce🅱sori🅱g the i🅱ter🅱et si🅱ce 1989");
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+
+  if (message.author.bot) {
+    return;
+  }
+
+  var ret = '';
+  for (var i = 0; i < message.content.length; i++) {
+     var chr = message.content.substring(i, i+1);
+     if (chr.toUpperCase() != 'N') {
+        ret += chr;
+     }
+  }
+
+  if (message.content.length != ret.length) {
+    message.channel.send('Did you mean "' + ret + '"? The letter :regional_indicator_n: is prohibited per orders of Supreme Leader Xi Jipig.');
+  }
+});
+
+client.login(auth.token);
