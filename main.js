04fe43d1abffac557849de4695ea7cb89f7004a4
Aditya Saligrama <saligrama427@gmail.com>
2018-03-01 13:25:19 -0500

    Fix playing

--- main.js
+++ main.js
@@ -22,13 +22,11 @@ client.on("ready", () => {
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
 
